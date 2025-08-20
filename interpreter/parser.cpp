bd14a6dc3c1d0ab9787db0127e6db3aeb5a79c11
栢兹氧 <baiziyang2022@yeah.net>
2025-08-20 15:17:22 +0800

    更新parser.cpp

--- interpreter/parser.cpp
+++ interpreter/parser.cpp
@@ -4,15 +4,10 @@
 #include <stdexcept>
 #include <string>
 
-#ifdef NDEBUG
-static const bool PARSER_DEBUG = false;
-#else
-static const bool PARSER_DEBUG = true;
-#endif
 
 // Debug output macro - no output if DEBUG is false
 #define DEBUG_OUT \
-    if (PARSER_DEBUG) std::cerr
+    if (false) std::cerr
 
 std::unique_ptr<Expression> Parser::parse_expression(const std::vector<Token>& tokens, size_t& i) {
     // Safety check
