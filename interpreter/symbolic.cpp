34d62e2b55b20c75bfefbfcb305b9b7e53e08994
steambird1 <77623231+steambird1@users.noreply.github.com>
2025-09-03 19:00:04 +0800

    Add files via upload

--- interpreter/symbolic.cpp
+++ interpreter/symbolic.cpp
@@ -647,7 +647,7 @@ std::shared_ptr<SymbolicExpr> SymbolicExpr::simplify_power() const {
 				// 化简成功
 				// TODO: Debug output:
 			std::cerr << "[Debug output] Power simplifying (rational ^ rational) - success" << std::endl;
-				return SymbolicExpr::power(SymbolicExpr::number((::Rational(bs_n, bs_d)).power(es_n)), SymbolicExpr::number(es_n));
+				return SymbolicExpr::power(SymbolicExpr::number((::Rational(bs_n, bs_d)).power(::BigInt(es_n))), SymbolicExpr::number(es_n));
 			}
 			// 否则化简失败，注意 bs_n 和 bs_d 可能需要重新获取
 		}
