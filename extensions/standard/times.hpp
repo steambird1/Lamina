03837565594137a68b06aa660fcadcdbb069c3f9
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-10-05 08:52:37 +0800

    更新 times.hpp

--- extensions/standard/times.hpp
+++ extensions/standard/times.hpp
@@ -1,15 +1,1 @@
-#ifndef TIMES_HPP
-#define TIMES_HPP
-#include "../../interpreter/lamina_api/lamina.hpp"
-
-Value get_time(const std::vector<Value>& args);
-Value get_date(const std::vector<Value>& args);
-Value get_format_date(const std::vector<Value>& args);
-
-namespace lamina {
-    LAMINA_FUNC("get_time", get_time, 0);
-    LAMINA_FUNC("get_date", get_date, 0);
-    LAMINA_FUNC("format_date", get_format_date, 1);
-}// namespace lamina
-
-#endif//TIMES_HPP
+// --null--
