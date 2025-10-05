1542ebd3d8cd5c9dcff5666dbe7518a580a0db2d
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-10-05 08:49:09 +0800

    更新 array.hpp

--- extensions/standard/array.hpp
+++ extensions/standard/array.hpp
@@ -1,14 +1,1 @@
-#ifndef ARRAY_HPP
-#define ARRAY_HPP
-#include "../../interpreter/lamina_api/lamina.hpp"
-
-Value range(const std::vector<Value>& args);
-Value visit_array_by_int(const std::vector<Value>& args);
-Value visit_array_by_str(const std::vector<Value>& args);
-
-namespace Lamina {
-    LAMINA_FUNC_WIT_ANY_ARGS("range", range)
-    LAMINA_FUNC_WIT_ANY_ARGS("visit", visit_array_by_int)
-    LAMINA_FUNC_WIT_ANY_ARGS("visit_by_str", visit_array_by_str)
-}// namespace Lamina
-#endif//ARRAY_HPP
+// --null--
