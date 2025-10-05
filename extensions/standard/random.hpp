56127b19538ddf0afe5e251e44d9fb469c1c5b37
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-10-05 08:51:59 +0800

    更新 random.hpp

--- extensions/standard/random.hpp
+++ extensions/standard/random.hpp
@@ -1,18 +1,1 @@
-#ifndef RANDOM_HPP
-#define RANDOM_HPP
-
-#include "../../interpreter/lamina_api/lamina.hpp"
-#include <vector>
-
-Value rand(const std::vector<Value>& args);
-Value randint(const std::vector<Value>& args);
-Value randstr(const std::vector<Value>& args);
-
-namespace lamina {
-    LAMINA_FUNC("rand", rand, 0);
-    LAMINA_FUNC("randint", randint, 2);
-    LAMINA_FUNC("randstr", randstr, 1);
-}// namespace lamina
-
-
-#endif// RANDOM_HPP
+// --null--
