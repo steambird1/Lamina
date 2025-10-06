5053d9cd68148f7c154cf5010d3fa33912cfc996
steambird1 <77623231+steambird1@users.noreply.github.com>
2025-10-06 13:42:06 +0800

    主分支添加调试输出

--- interpreter/eval.cpp
+++ interpreter/eval.cpp
@@ -230,6 +230,9 @@ Value Interpreter::eval_BinaryExpr(const BinaryExpr* bin) {
             return l.vector_add(r);
         }
         // 只要有一方是 Irrational 或 Symbolic，优先生成符号表达式
+		// TODO: Debug output:
+		std::cerr << "Adding: l type " << l->type << "; r type " << r->type << std::endl;
+		
         if ((l.is_irrational() || l.is_symbolic() || r.is_irrational() || r.is_symbolic()) && l.is_numeric() && r.is_numeric()) {
             std::shared_ptr<SymbolicExpr> leftExpr;
             std::shared_ptr<SymbolicExpr> rightExpr;
